d37440cc91a057a968b0dc8fc18d98f4e8a875cf
Luis Santos <lsantos.dev@gmail.com>
2018-12-01 15:48:34 +0000

    write_data(); adding documentation

--- datasets/util/util.py
+++ datasets/util/util.py
@@ -10,9 +10,19 @@ SENT_BOUNDARY = np.nan
 IS_SENT_BOUNDARY = lambda val: pd.isna(val)
 NOT_SENT_BOUNDARY = lambda val: pd.notna(val)
 
-def read_data(token_generator, token_re):
-    # create a Series of raw tokens by means of a token generator on the input dataset file
-    raw_tokens = pd.Series(token_generator)
+def read_data(raw_tokens, token_re):
+    """
+    Creates a DataFrame containing the tokens as the rows and token information
+    such as the PoS/NER tag or lemma of the token as columns. To indicate sentence
+    boundaries the special token SENT_BOUNDARY should be used between sentences.
+
+    :param raw_tokens: An iterable (typically a generator function called over the
+    input dataset file) that yields tokens and yields SENT_BOUNDARY between sentences.
+    :param token_re: A regex to extract the token attributes from the raw token by
+    means of capture groups. Each capture group in the regex becomes a new column.
+    """
+    # create a Series of raw tokens
+    raw_tokens = pd.Series(raw_tokens)
 
     # make sure the token regex matches the whole token
     assert raw_tokens.str.match(token_re).all(), 'Token regex failed to match at least one token'
@@ -25,7 +35,7 @@ def read_data(token_generator, token_re):
 
 def sent_counts(data_df):
     """
-    Retrieves number of tokens and number of sentences from a DataFrame
+    Retrieves number of tokens and number of sentences from a DataFrame.
     """
     num_tokens = data_df.Token.count()  # counts non-NaN rows
     num_sents = len(data_df) - num_tokens  # counts NaN rows
@@ -70,6 +80,14 @@ def _sents_to_dataframe(sents, sent_boundary_token):
         yield sent_boundary_token
 
 def train_test_split(data_df, data_split):
+    """
+    Splits the input data into train, dev and tests sets. The order
+    of the sentences is randomized before making the split.
+
+    :param data_df: A DataFrame containing the input data to split on.
+    :param data_split: A dictionary containing the proportions of the
+    train, dev and test sets as a percentage, which must sum to one.
+    """
     assert sum(data_split.values()) == 1.0, 'Total proportion of train, dev and test splits must sum to 1.0'
 
     sents = dataframe_to_sents(data_df)
@@ -95,10 +113,17 @@ def train_test_split(data_df, data_split):
 
     return data_splitted
 
-
 def write_data(data_df, dataset_out_path, output_separator):
-    # define output format as joining columns with an output separator (e.g., ' ', '\t')
-    # in a line-based format of a token per line where empty lines denote sentence boundaries
-    output_rows = data_df.Token + output_separator + data_df.POS
+    """
+    Writes the data to a file in a format similar in scope to CoNLL-2003.
+    The output file will contain one token per line, where empty lines denote
+    sentence boundaries. Token attributes are joined by an output separator.
+    """
+    # concatenate the Token column with all other columns via an output separator
+    output_rows = data_df.Token.str.cat(others=data_df.drop(columns='Token'),
+                                        sep=output_separator)
+
+    # separate tokens by newline characters where NaN rows become empty lines
     output_text = output_rows.str.cat(sep='\n', na_rep='')
     dataset_out_path.write_text(output_text)
+
