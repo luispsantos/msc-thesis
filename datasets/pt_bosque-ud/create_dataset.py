8a774d210997fc15e8275bdaf32db6a02a1c8866
Luis Santos <luis@paiva.eu>
2019-02-09 22:32:59 +0000

    renaming dataset from pt_bosque to pt_bosque-ud

--- datasets/pt_bosque-ud/create_dataset.py
+++ datasets/pt_bosque-ud/create_dataset.py
@@ -9,8 +9,6 @@ os.chdir(sys.path[0])
 
 # importing from util/ directory
 sys.path.insert(1, str(Path.cwd().parent / 'util'))
-from rule_matcher import RuleMatcher
-import contractions
 from util import *
 
 #read variables from the configuration file
@@ -19,23 +17,13 @@ with open('config.yml', 'r') as f:
 
 dataset_in_dir, dataset_out_dir = Path(config['dataset_in_dir']), Path(config['dataset_out_dir'])
 output_separator, output_columns  = config['output_separator'], config['output_columns']
-keep_contractions = config['keep_contractions']
-
-# create output dataset directory if it doesn't exist
-if not dataset_out_dir.exists():
-    dataset_out_dir.mkdir(parents=True)
-
-matcher = RuleMatcher(contractions.rule_list)
 
 for dataset_type in ['train', 'dev', 'test']:
     dataset_in_path = dataset_in_dir / 'pt_bosque-ud-{}.conllu'.format(dataset_type)
     dataset_out_path = dataset_out_dir / '{}.txt'.format(dataset_type)
 
     data_df = read_conllu(dataset_in_path)
+    data_df = extract_multiwords(data_df)
 
-    # make contractions
-    if keep_contractions:
-        data_df = matcher.apply_rules(data_df)
-
-    write_data(data_df, dataset_out_path, output_separator)
+    write_data(data_df, dataset_out_path, output_separator, output_columns)
     print('Created file {}'.format(dataset_out_path))
