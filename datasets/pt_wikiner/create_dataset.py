38c7179671f76dd2bfe0d7a9512db925402253b9
Luis Santos <luis@paiva.eu>
2019-03-17 23:11:51 +0000

    adding WikiNER PT

---
+++ datasets/pt_wikiner/create_dataset.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import pandas as pd
+import yaml
+import re
+import sys
+import os
+
+# change working dir into the directory containing the script
+os.chdir(sys.path[0])
+
+# importing from util/ directory
+sys.path.insert(1, str(Path.cwd().parent / 'util'))
+from util import *
+
+# read variables from the configuration file
+with open('config.yml', 'r') as f:
+    config = yaml.load(f)
+
+dataset_in_dir, dataset_out_dir = Path(config['dataset_in_dir']), Path(config['dataset_out_dir'])
+output_columns = config['output_columns']
+
+# read dataset-specific rules
+with open('rules.yml', 'r') as f:
+    rules = yaml.load(f)
+
+pos_tagset_ud_map = rules['pos_tagset_ud_map']
+
+wikiner_file = dataset_in_dir / 'aij-wikiner-pt-wp3'
+token_re = re.compile('^(?P<Token>.+?)\|(?P<POS>[A-Za-z_+]+)\|(?P<NER>[A-Z-]+)$')
+
+# open WikiNER file and remove empty lines
+with wikiner_file.open('r') as f:
+    sents = [sent for sent in f if sent.strip()]
+    data_df = read_data(sents, token_re)
+
+# convert tags to UD tagset
+data_df['UPOS'] = data_df.POS.map(pos_tagset_ud_map)
+
+# split data into train, dev and test sets and write data to disk
+train_test_dfs = train_test_split(data_df)
+write_data(train_test_dfs, dataset_out_dir, output_columns)
+
