7cfaef3fc33d5ba369c9a7dd5ec3819eeb19f808
Luis Santos <luis@paiva.eu>
2019-03-17 23:00:17 +0000

    updating Mac-Morpho

--- datasets/pt_mac-morpho/create_dataset.py
+++ datasets/pt_mac-morpho/create_dataset.py
@@ -1,5 +1,5 @@
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 import yaml
 import re
 import sys
@@ -18,33 +18,30 @@ with open('config.yml', 'r') as f:
     config = yaml.load(f)
 
 dataset_in_dir, dataset_out_dir = Path(config['dataset_in_dir']), Path(config['dataset_out_dir'])
-output_separator, output_columns  = config['output_separator'], config['output_columns']
+output_columns = config['output_columns']
 
 # read dataset-specific rules
 with open('rules.yml', 'r') as f:
     rules = yaml.load(f)
 
-rules, pos_tagset_ud_map = rules['rules'], rules['pos_tagset_ud_map']
-matcher = RuleMatcher(rules)
+pos_tagset_ud_map, rules = rules['pos_tagset_ud_map'], rules['rules']
 
+matcher = RuleMatcher(rules)
 token_re = re.compile('^(?P<Token>.+?)_(?P<POS>[A-Z+-]+)$')
 
-def token_generator(dataset_in_path):
-    for sent in dataset_in_path.open('r'):
-        for token in sent.split():
-            yield token
-        yield SENT_BOUNDARY
-
 for dataset_type in ['train', 'dev', 'test']:
-    dataset_in_path = dataset_in_dir / 'macmorpho-{}.txt'.format(dataset_type)
-    dataset_out_path = dataset_out_dir / '{}.txt'.format(dataset_type)
+    data_in_path = dataset_in_dir / f'macmorpho-{dataset_type}.txt'
+    data_out_path = dataset_out_dir / f'{dataset_type}.txt'
 
-    data_df = read_data(token_generator(dataset_in_path), token_re)
+    # read sentences to a DataFrame
+    with data_in_path.open('r') as f:
+        sents = f.readlines()
+        data_df = read_data(sents, token_re)
 
     # convert the POS tagset
     data_df['UPOS'] = data_df.POS.map(pos_tagset_ud_map)
     data_df, rule_counts = matcher.apply_rules(data_df)
 
-    write_data(data_df, dataset_out_path, output_separator, output_columns)
-    print('Created file {}'.format(dataset_out_path))
+    # write data to disk
+    write_data(data_df, data_out_path, output_columns)
 
