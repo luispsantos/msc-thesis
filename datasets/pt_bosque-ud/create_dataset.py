118252f90b5bdf8ce937aa0cd6baa7a5432ea468
Luis Santos <luis@paiva.eu>
2019-03-18 15:48:46 +0000

    updating Bosque

--- datasets/pt_bosque-ud/create_dataset.py
+++ datasets/pt_bosque-ud/create_dataset.py
@@ -1,5 +1,5 @@
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 import yaml
 import sys
 import os
@@ -16,14 +16,22 @@ with open('config.yml', 'r') as f:
     config = yaml.load(f)
 
 dataset_in_dir, dataset_out_dir = Path(config['dataset_in_dir']), Path(config['dataset_out_dir'])
-output_separator, output_columns  = config['output_separator'], config['output_columns']
+output_columns = config['output_columns']
+
+# read dataset-specific rules
+with open('rules.yml', 'r') as f:
+    rules = yaml.load(f)
+
+multiword_upos_map = rules['multiword_upos_map']
 
 for dataset_type in ['train', 'dev', 'test']:
-    dataset_in_path = dataset_in_dir / 'pt_bosque-ud-{}.conllu'.format(dataset_type)
-    dataset_out_path = dataset_out_dir / '{}.txt'.format(dataset_type)
+    data_in_path = dataset_in_dir / f'pt_bosque-ud-{dataset_type}.conllu'
+    data_out_path = dataset_out_dir / f'{dataset_type}.txt'
 
-    data_df = read_conllu(dataset_in_path)
-    data_df = extract_multiwords(data_df)
+    # read CoNLL-U data and extract multi-word tokens
+    data_df = read_conllu(data_in_path)
+    data_df = extract_multiwords(data_df, multiword_upos_map)
 
-    write_data(data_df, dataset_out_path, output_separator, output_columns)
-    print('Created file {}'.format(dataset_out_path))
+    # write data to disk
+    write_data(data_df, data_out_path, output_columns)
+
