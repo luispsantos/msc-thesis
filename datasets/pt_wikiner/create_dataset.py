f86758832c022a933e1f833396ec6a6a182aa9bc
Luis Santos <luis@paiva.eu>
2019-03-26 16:45:59 +0000

    revised POS and NER conversion rules for WikiNER PT

--- datasets/pt_wikiner/create_dataset.py
+++ datasets/pt_wikiner/create_dataset.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import pandas as pd
-import yaml
 import re
 import sys
 import os
@@ -8,21 +7,17 @@ import os
 # change working dir into the directory containing the script
 os.chdir(sys.path[0])
 
-# importing from util/ directory
-sys.path.insert(1, str(Path.cwd().parent / 'util'))
+# importing util package from parent directory
+sys.path.insert(1, str(Path.cwd().parent))
 from util import *
 
 # read variables from the configuration file
-with open('config.yml', 'r') as f:
-    config = yaml.load(f)
-
+config = load_yaml('config.yml')
 dataset_in_dir, dataset_out_dir = Path(config['dataset_in_dir']), Path(config['dataset_out_dir'])
 output_columns = config['output_columns']
 
 # read dataset-specific rules
-with open('rules.yml', 'r') as f:
-    rules = yaml.load(f)
-
+rules = load_yaml('rules.yml')
 pos_tagset_ud_map = rules['pos_tagset_ud_map']
 
 wikiner_file = dataset_in_dir / 'aij-wikiner-pt-wp3'
@@ -31,10 +26,15 @@ token_re = re.compile('^(?P<Token>.+?)\|(?P<POS>[A-Za-z_+]+)\|(?P<NER>[A-Z-]+)$'
 # open WikiNER file and remove empty lines
 with wikiner_file.open('r') as f:
     sents = [sent for sent in f if sent.strip()]
-    data_df = read_data(sents, token_re)
+
+# read WikiNER data into a DataFrame
+data_df = read_data(sents, token_re)
 
 # convert tags to UD tagset
 data_df['UPOS'] = data_df.POS.map(pos_tagset_ud_map)
+
+# convert IOB tagging scheme to BIO
+iob_to_bio(data_df.NER)
 
 # split data into train, dev and test sets and write data to disk
 train_test_dfs = train_test_split(data_df)
