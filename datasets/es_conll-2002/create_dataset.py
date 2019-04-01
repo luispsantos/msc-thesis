1dd40b8122a36b723551a2b5882185b8f55fbf5b
Luis Santos <luis@paiva.eu>
2019-04-01 15:52:30 +0100

    updating CoNLL-02

--- datasets/es_conll-2002/create_dataset.py
+++ datasets/es_conll-2002/create_dataset.py
@@ -1,34 +1,23 @@
 from pathlib import Path
 import pandas as pd
-import yaml
 import sys
 import os
-import csv
 
 # change working dir into the directory containing the script
 os.chdir(sys.path[0])
 
-# importing from util/ directory
-sys.path.insert(1, str(Path.cwd().parent / 'util'))
+# importing util package from parent directory
+sys.path.insert(1, str(Path.cwd().parent))
 from util import *
 
 # read variables from the configuration file
-with open('config.yml', 'r') as f:
-    config = yaml.load(f)
-
+config = load_yaml('config.yml')
 dataset_in_dir, dataset_out_dir = Path(config['dataset_in_dir']), Path(config['dataset_out_dir'])
 output_columns = config['output_columns']
 
 # read dataset-specific rules
-with open('rules.yml', 'r') as f:
-    rules = yaml.load(f)
-
+rules = load_yaml('rules.yml')
 pos_tagset_ud_map = rules['pos_tagset_ud_map']
-
-def read_conll(text_file):
-    data_df = pd.read_csv(text_file, sep=' ', names=['Token', 'POS', 'NER'],
-                          skip_blank_lines=False, quoting=csv.QUOTE_NONE)
-    return data_df
 
 conll_files = {'train': 'esp.train', 'dev': 'esp.testa', 'test': 'esp.testb'}
 
@@ -37,7 +26,7 @@ for dataset_type, conll_file in conll_files.items():
     data_out_path = dataset_out_dir / (dataset_type + '.txt')
 
     # read CoNLL-2002 data
-    data_df = read_conll(data_in_path)
+    data_df = read_conll(data_in_path, sep=' ', column_names=['Token', 'POS', 'NER'])
 
     # obtain the first and first+second characters of the POS tag (e.g. VMIS3S0 -> V and VM)
     pos = data_df.POS.astype('category')
