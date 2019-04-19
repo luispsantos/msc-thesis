9b8fe4671f90d8d3686ec392c16771b24492804f
Luis Santos <lsantos.dev@gmail.com>
2019-04-19 20:08:07 +0100

    rewrote code for training on single task scenario

---
+++ emnlp2017-bilstm-cnn-crf/train_single_task.py
@@ -0,0 +1,82 @@
+import os
+import logging
+import sys
+from pathlib import Path
+from neuralnets.BiLSTM import BiLSTM
+from util.preprocessing import prepareDataset, loadDatasetPickle
+from util.datasets import Datasets
+
+# :: Change into the working dir of the script ::
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# :: Logging level ::
+loggingLevel = logging.INFO
+logger = logging.getLogger()
+logger.setLevel(loggingLevel)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(loggingLevel)
+formatter = logging.Formatter('%(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# paths for input/output directories
+embeddings_dir = Path('embeddings')
+models_dir, results_dir = Path('models/single_task'), Path('results/single_task')
+
+# BiLSTM network hyperparameters
+network_params = {'charEmbeddings': 'CNN', 'LSTM-Size': [100, 100], 'classifier': 'CRF',
+                  'dropout': (0.5, 0.5), 'earlyStopping': 5, 'miniBatchSize': 32}
+
+# number of runs with different random initialization
+max_runs = 1
+
+def run_experiment(dataset_id, dataset_dict, task, embeddings_path, max_runs):
+    # prepares the dataset to be used with the LSTM-network
+    # creates and stores pickle files in the pkl/ folder
+    pickleFile = prepareDataset(embeddings_path, dataset_dict, None, True)
+
+    # loads the embeddings and the dataset
+    embeddings, mappings, data = loadDatasetPickle(pickleFile)
+
+    experiment_name = f'{dataset_id}_{task.lower()}'
+
+    # run the model multiple times with the same hyperparameters and different random initialization
+    for run_idx in range(max_runs): 
+        # set network hyperparameters and mappings/datasets
+        model = BiLSTM(network_params)
+        model.setMappings(mappings, embeddings)
+        model.setDataset(dataset_dict, data)
+
+        # path to store the trained model and model results
+        model.modelSavePath = models_dir / f'{experiment_name}.h5'
+        model.storeResults(results_dir / f'{experiment_name}.csv')
+
+        # build and train the model
+        model.buildModel()
+        model.fit(epochs=500)  # do not limit training by epochs - use early stopping
+
+datasets = Datasets()
+
+# iterate through the multiple Portuguese and Spanish datasets
+for dataset_id, dataset in datasets:
+    # obtain the dataset's language
+    lang = dataset.lang
+
+    # select fasttext word embeddings
+    embeddings_path = embeddings_dir / f'{lang.lower()}.fasttext.oov.vec.gz'
+
+    # iterate through the tasks (POS and NER) for a dataset
+    for task, dataset_dict in dataset:
+        # run experiment in a separate process so that memory is released at the end
+        pid = os.fork()
+        if pid == 0:  # child process
+            run_experiment(dataset_id, dataset_dict, task, embeddings_path, max_runs)
+            sys.exit(0)
+        else:  # parent process
+            pid, status = os.waitpid(pid, 0)
+            assert status == 0, f'Child process returned with failed status code: {status}'
+            logger.info(f'Completed experiment: {dataset_id} - {task}')
+
--- emnlp2017-bilstm-cnn-crf/util/datasets.py
+++ emnlp2017-bilstm-cnn-crf/util/datasets.py
@@ -8,21 +8,26 @@ class Dataset:
     # map dataset column names to names expected by the application
     column_map = {'Token': 'tokens', 'UPOS': 'POS', 'NER': 'NER_BIO'}
 
-    def __init__(self, dataset_name):
-        self.dataset_name = dataset_name
-        self.lang = dataset_name.split('_')[0]
-        self.validate(dataset_name)
+    def __init__(self, dataset_id):
+        self.dataset_id = dataset_id
+        self.validate(dataset_id)
+
+        lang = dataset_id.split('_')[0]
+        self.lang = lang.upper()
 
         columns = self.config['columns']
+        self.name = self.config['name']
+
         columns = [self.column_map.get(col, col) for col in columns]
-
         self.columns = {idx: col for idx, col in enumerate(columns)}
-        self.tasks = [col for col in columns if col in ('POS', 'NER_BIO')]
 
-    def validate(self, dataset_name):
-        dataset_path = Datasets.datasets_dir / dataset_name
+        self.labels = [col for col in columns if col in ('POS', 'NER_BIO')]
+        self.tasks = [col.replace('_BIO', '') for col in self.labels]
+
+    def validate(self, dataset_id):
+        dataset_path = Datasets.datasets_dir / dataset_id
         if not dataset_path.exists():
-            raise InvalidDatasetError(f'Dataset {dataset_name} does not exist at {dataset_path}')
+            raise InvalidDatasetError(f'Dataset {dataset_id} does not exist at {dataset_path}')
 
         for dataset_type in ['train', 'dev', 'test']:
             data_file = dataset_path / (dataset_type + '.txt')
@@ -36,21 +41,26 @@ class Dataset:
         except Exception:
             raise InvalidDatasetError(f'Cannot open configuration file {config_file}')
 
+    def __iter__(self):
+        for task in self.tasks:
+            yield task, self.to_dict(task)
+
     def to_dict(self, task=None):
-        if task is None:
-            tasks = self.tasks
-        elif task not in self.columns.values():
-            raise InvalidDatasetError(f'Cannot use label {task} for prediction')
+        label = task + '_BIO' if task == 'NER' else task
+        if label is None:
+            predict_label = self.labels
+        elif label in self.labels:
+            predict_label = [label]
         else:
-            tasks = [task]
+            raise InvalidDatasetError(f'Cannot use label {label} for prediction')
 
         dataset_params = {
             'columns': self.columns,
-            'label': tasks,
+            'label': predict_label,
             'evaluate': True,
             'commentSymbol': None
         }
-        dataset = {self.dataset_name: dataset_params}
+        dataset = {self.dataset_id: dataset_params}
 
         return dataset
 
@@ -59,23 +69,22 @@ class Datasets:
 
     def __init__(self, names=None, exclude=None, lang=None, task=None):
         self.datasets = {}
-        task = task + '_BIO' if task == 'NER' else task
         self.lang, self.task = lang, task
 
         # iterate through the multiple datasets
         for dataset_path in sorted(self.datasets_dir.iterdir()):
-            dataset_name = dataset_path.name
-            dataset = Dataset(dataset_name)
+            dataset_id = dataset_path.name
+            dataset = Dataset(dataset_id)
 
             # selectively choose the datasets
-            if (names is None or dataset_name in names) \
-            and (exclude is None or dataset_name not in exclude) \
-            and (lang is None or lang == dataset.lang)  \
+            if (names is None or dataset_id in names) \
+            and (exclude is None or dataset_id not in exclude) \
+            and (lang is None or lang == dataset.lang) \
             and (task is None or task in dataset.tasks):
-                self.datasets[dataset_name] = dataset
+                self.datasets[dataset_id] = dataset
 
     def __iter__(self):
-        return self.datasets.items()
+        yield from self.datasets.items()
 
     def to_dict(self):
         datasets = {}
