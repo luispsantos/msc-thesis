2a6b3496a68fcebe641440c875858b9fb3ab1892
Luis Santos <lsantos.dev@gmail.com>
2019-04-20 10:33:56 +0100

    renamed train_multitask.py to train_multi_task.py

--- emnlp2017-bilstm-cnn-crf/train_multi_task.py
+++ emnlp2017-bilstm-cnn-crf/train_multi_task.py
@@ -24,25 +24,25 @@ logger.addHandler(ch)
 
 # paths for input/output directories
 embeddings_dir = Path('embeddings')
-models_dir, results_dir = Path('models/multitask'), Path('results/multitask')
+models_dir, results_dir = Path('models/multi_task'), Path('results/multi_task')
 
 # select network hyperparameters
 network_params = {'charEmbeddings': 'CNN', 'LSTM-Size': [100, 100], 'classifier': 'CRF',
-                  'dropout': (0.5, 0.5), 'earlyStopping': 5, 'miniBatchSize': 32}
+                  'dropout': (0.5, 0.5), 'earlyStopping': 15, 'miniBatchSize': 32}
 
 # number of runs with different random initialization
 max_runs = 1
 
-def run_experiment(datasets, embeddings_path, lang, task, max_runs):
+def run_experiment(datasets, lang, task, embeddings_path, max_runs):
     # prepares the datasets to be used with the LSTM-network
     # creates and stores pickle files in the pkl/ folder
     pickleFile = prepareDataset(embeddings_path, datasets, None, True)
 
-    # load the embeddings and the dataset
+    # loads the embeddings and the dataset
     embeddings, mappings, data = loadDatasetPickle(pickleFile)
 
-    lang_prefix = f'{lang}_' if lang is not None else ''
-    task_suffix = f'_{task}' if task is not None else ''
+    lang_prefix = f'{lang.lower()}_' if lang is not None else ''
+    task_suffix = f'_{task.lower()}' if task is not None else ''
     experiment_name = lang_prefix + 'datasets' + task_suffix
 
     # run the model multiple times with the same hyperparameters and different random initialization
@@ -58,22 +58,27 @@ def run_experiment(datasets, embeddings_path, lang, task, max_runs):
 
         # build and train the model
         model.buildModel()
-        model.fit(epochs=1)  # do not limit training by epochs - use early stopping
+        model.fit(epochs=500)  # do not limit training by epochs - use early stopping
 
 
-multitask_datasets = [Datasets(names=['pt_bosque-ud', 'pt_cintil'], lang='pt', task='POS')]
+multitask_datasets = [
+    Datasets(exclude=['pt_colonia', 'pt_wikiner'], lang='PT', task='POS'),
+    Datasets(exclude=['pt_wikiner'], lang='PT', task='NER'),
+    Datasets(exclude=['pt_colonia', 'pt_wikiner'], lang='PT')
+]
 
-# iterate through the multiple dataset language and task combinations
+# iterate through the multiple language and task dataset combinations
 for datasets in multitask_datasets:
     # obtain datasets' language and task
     lang, task = datasets.lang, datasets.task
 
     # select fasttext word embeddings
-    embeddings_path = embeddings_dir / f'{lang}.fasttext.oov.vec.gz'
+    embeddings_path = embeddings_dir / f'{lang.lower()}.fasttext.oov.vec.gz'
 
     pid = os.fork()
     if pid == 0:  # child process
-        run_experiment(datasets.to_dict(), embeddings_path, lang, task, max_runs)
+        # run experiment in a separate process so that memory is released at the end
+        run_experiment(datasets.to_dict(), lang, task, embeddings_path, max_runs)
         sys.exit(0)
     else:  # parent process
         pid, status = os.waitpid(pid, 0)
