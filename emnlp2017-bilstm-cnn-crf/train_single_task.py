4a0ac8fb802f155655439923e5b536ae3b8c8f6d
Luis Santos <lsantos.dev@gmail.com>
2019-05-15 21:26:02 +0000

    fixing single task hyperparameters

--- emnlp2017-bilstm-cnn-crf/train_single_task.py
+++ emnlp2017-bilstm-cnn-crf/train_single_task.py
@@ -27,8 +27,8 @@ embeddings_dir = Path('embeddings')
 models_dir, results_dir = Path('models/single_task'), Path('results/single_task')
 
 # BiLSTM network hyperparameters
-network_params = {'charEmbeddings': 'CNN', 'LSTM-Size': [100, 100], 'classifier': 'CRF',
-                  'dropout': (0.5, 0.5), 'earlyStopping': 5, 'miniBatchSize': 32}
+network_params = {'charEmbeddings': 'LSTM', 'charLSTMSize': 25, 'LSTM-Size': [100, 100],
+                  'classifier': 'CRF','dropout': (0.5, 0.5), 'earlyStopping': 10}
 
 # number of runs with different random initialization
 max_runs = 1
